959fa801f6680e176dee9c7a32e4f9ee38caebbe
fhemery <f.hemery@hemit.fr>
2022-11-29 13:08:48 +0100

    fix(tests): fix configuration so that jest works with firebase.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  resolver: '<rootDir>/jest.resolver.js',
+  transformIgnorePatterns: [
+    'node_modules/(?!@angular|@firebase|firebase|@ngrx)',
+  ],
 };
--- src/app/pages/favorites-page/favorites-page.component.spec.ts
+++ src/app/pages/favorites-page/favorites-page.component.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoritesPageComponent } from './favorites-page.component';
+import { FavoriteService } from '../../services/favorite.service';
+import { of } from 'rxjs';
 
 describe('FavoritesPageComponent', () => {
   let component: FavoritesPageComponent;
   let fixture: ComponentFixture<FavoritesPageComponent>;
+  let favoriteService: FavoriteService;
 
   beforeEach(async () => {
+    favoriteService = {
+      getFavorites: () => of([]),
+    } as unknown as FavoriteService;
     await TestBed.configureTestingModule({
-      declarations: [ FavoritesPageComponent ]
-    })
-    .compileComponents();
+      declarations: [FavoritesPageComponent],
+      providers: [
+        {
+          provide: FavoriteService,
+          useValue: favoriteService,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FavoritesPageComponent);
     component = fixture.componentInstance;
