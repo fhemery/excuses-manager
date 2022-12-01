d8572a31c09ad3f3b39d23e891a4dbe585702012
fhemery <f.hemery@hemit.fr>
2022-12-01 14:24:23 +0100

    feat(pwa): add caching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,13 +36,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     TranslateModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule,
+    AngularFirestoreModule.enablePersistence(),
     FooterModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
   providers: [],
